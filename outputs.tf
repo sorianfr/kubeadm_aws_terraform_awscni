c7ca4e96d008ff3d4b9c8105bc2419438f8b1034
Javier Soriano <sorianfr@icloud.com>
2024-11-20 08:36:37 +0100

    Initial commit

---
+++ outputs.tf
@@ -0,0 +1,16 @@
+
+output "bastion_public_ip" {
+  value = aws_instance.bastion.public_ip
+}
+
+output "controlplane_private_ip" {
+  value = aws_instance.controlplane.private_ip
+}
+
+output "worker1_private_ip" {
+  value = aws_instance.worker1.private_ip
+}
+
+output "worker2_private_ip" {
+  value = aws_instance.worker2.private_ip
+}
