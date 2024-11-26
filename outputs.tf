f2c792178db3aefc1e405cb6924003ebb909c4d3
Javier Soriano <114910690+sorianfr@users.noreply.github.com>
2024-11-26 09:20:31 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,10 @@ output "bastion_public_ip" {
   value = aws_instance.bastion.public_ip
 }
 
+output "bastion_public_dns" {
+  value = aws_instance.bastion.public_dns
+}
+
 output "controlplane_private_ip" {
   value = aws_instance.controlplane.private_ip
 }
